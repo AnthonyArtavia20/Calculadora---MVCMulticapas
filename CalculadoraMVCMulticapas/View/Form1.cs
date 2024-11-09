aa4a2e95eb709eb3dee85e001017764a90a07091
Anthony <anthony.artavia20@gmail.com>
2024-11-09 00:46:04 -0600

    Primera estructuración de la tarea MultiCapasMVC

---
+++ CalculadoraMVCMulticapas/View/Form1.cs
@@ -0,0 +1,40 @@
+namespace CalculadoraMVCMulticapas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
